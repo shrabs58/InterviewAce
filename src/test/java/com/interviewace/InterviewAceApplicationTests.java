510b95b484338d239890c35b685993f50356da07
Shraddha <shraddhapolanki@gmail.com>
2026-06-26 23:47:15 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/interviewace/InterviewAceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
